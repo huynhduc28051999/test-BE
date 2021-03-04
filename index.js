f621ef9549876d00538df26a0f1aeb29a42a3d82
huynhduc280519999 <huynhduc28051999@gmail.com>
2021-03-04 19:28:18 +0700

    add

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 4000
+
+const authors = ['tac gia 1', 'tac gia 2', 'tac gia 3']
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/authors', (req, res) => {
+  res.send(authors)
 })
 
 app.listen(port, () => {
